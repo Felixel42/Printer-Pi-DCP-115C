78d9b9e9ae8470f4fc56adf545825890a5da3eb2
Felix <31103580+Felixel42@users.noreply.github.com>
2018-02-28 19:55:03 +0100

    has to be an absolute path

--- Scripts/Printer_OFF_Check.py
+++ Scripts/Printer_OFF_Check.py
@@ -8,4 +8,4 @@ os.system("lpinfo -v | grep -e 'direct usb*' > ONTest")
 os.system("lpstat -p | grep -e 'idle' > IdleTest")
 
 if os.stat("ONTest").st_size != 0 and os.stat("IdleTest").st_size != 0:
-    os.system("~/433Utils/RPi_utils/steuerung 0")
+    os.system("/home/pi/433Utils/RPi_utils/steuerung 0")
--- Scripts/Printer_ON_Check.py
+++ Scripts/Printer_ON_Check.py
@@ -6,4 +6,4 @@ os.chdir(".")
 os.system("lpinfo -v | grep -e 'direct usb*' > ONTest1")
 
 if os.stat("ONTest1").st_size == 0:
-    os.system("~/433Utils/RPi_utils/steuerung 1")
+    os.system("/home/pi/433Utils/RPi_utils/steuerung 1")
